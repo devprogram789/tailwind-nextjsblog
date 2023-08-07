0edc83e5b119f1b8ccffd40152c79fede3a6e4cd
VIP <88263706+UFAPRO888s@users.noreply.github.com>
2023-08-08 06:07:37 +0700

    Update about-us.js

--- pages/about-us.js
+++ pages/about-us.js
@@ -80,7 +80,7 @@ export default function About({ authorDetails }) {
               </p>
             </div>
           </div>
-          <div className="grid grid-cols-3 gap-10 py-4">
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-10 py-4">
             {Dataaaaa.map((dax) => (
               <div key={dax.id} className="text-center">
                 <Image
