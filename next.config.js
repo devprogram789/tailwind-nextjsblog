e1a00f5139cf9916254b7808cbc33b71ccf6f7ef
VIP <88263706+UFAPRO888s@users.noreply.github.com>
2023-08-20 15:32:49 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -94,7 +94,7 @@ module.exports = withBundleAnalyzer({
       // Replace React with Preact only in client production build
       Object.assign(config.resolve.alias, {
         'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
+         react: 'preact/compat',
         'react-dom/test-utils': 'preact/test-utils',
         'react-dom': 'preact/compat',
       })
